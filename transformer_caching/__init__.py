699f3690629b229c92891a1105298002ba3051bc
lmjantch <lassejantsch@web.de>
2025-11-17 21:00:52 +0900

    added TrasnformerCache to __init__.py, removed padding from default behavior for stack_tensors()

--- transformer_caching/__init__.py
+++ transformer_caching/__init__.py
@@ -1,2 +1,3 @@
 from .caching_wrapper import caching_wrapper
+from .transformer_cache import TransformerCache
 from .decomposition import *
--- transformer_caching/transformer_cache.py
+++ transformer_caching/transformer_cache.py
@@ -9,7 +9,11 @@ class TransformerCache(DataDict):
         self.model_config = model_config
         super().__init__()
 
-    def stack_tensors(self) -> None:
+    def stack_tensors(self, padding=False) -> None:
         for key, value in self.default_entry.items():
             if isinstance(value, torch.Tensor):
-                self.attach(key, max_pad_sequence(self[key]), force=True)
+                if padding:
+                    self[key] = max_pad_sequence(self[key])
+                else:
+                    self[key] = torch.stack(self[key])
+                    
