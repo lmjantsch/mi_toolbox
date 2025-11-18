0b18e67f0af2a3c53029eb6ee79d6b1078dec524
lmjantsch <lassejantsch@web.de>
2025-11-19 05:45:02 +0900

    added TransformerCache initialization through model_id

--- transformer_caching/transformer_cache.py
+++ transformer_caching/transformer_cache.py
@@ -1,12 +1,15 @@
 import torch
+from transformers import PretrainedConfig, AutoConfig
 
 from mi_toolbox.utils.collate.utils import max_pad_sequence
 from mi_toolbox.utils.data_types import DataDict
 
 class TransformerCache(DataDict):
 
-    def __init__(self, model_config) -> None:
+    def __init__(self, model_config: PretrainedConfig = None, model_id: str = None) -> None:
         self.model_config = model_config
+        if model_id and not model_config:
+            self.model_config = AutoConfig.from_pretrained(model_id)
         super().__init__()
 
     def stack_tensors(self, padding=False) -> None:
