fixed 'top_x_contribution_values' as there was a error where negative values where propagated, changed 'get_dot_prod_contribution' to accept other broadcastable shapes, added is_broadcastable function

contribution_tracing/mixin_measures.py
@@ -1,6 +1,8 @@
 import torch
 from typing import Union
 
+from .utils import is_broadcastable
+
 def get_dot_prod_contribution(parts: torch.Tensor, whole: torch.Tensor, device: Union[str, torch.device] = None) -> torch.Tensor:
     whole_sh = whole.shape
     parts_sh = parts.shape
@@ -8,8 +10,12 @@ def get_dot_prod_contribution(parts: torch.Tensor, whole: torch.Tensor, device: 
     
     if batch_dims < 0:
         raise ValueError(f"The 'parts' {tuple(parts_sh)} must have at least the same number of dimensions as the 'whole' {tuple(whole_sh)}")
-    if parts_sh[batch_dims:] != whole_sh:
-        raise ValueError(f"The non-batrch shape of 'parts' {tuple(parts_sh[batch_dims:])} must be the same as the shape of 'whole' {tuple(whole_sh)}")
+    
+    if whole_sh[-1] != parts_sh[-1]:
+        raise ValueError(f"The reduction dimension of 'parts' {tuple(parts_sh)} and 'whole' {tuple(whole_sh)} must be the same.")
+    
+    if not is_broadcastable(parts_sh, whole_sh):
+        raise ValueError(f"The 'parts' tensor {tuple(parts_sh)} and 'whole' tensor {tuple(whole_sh)} are not broadcastable.")
     
     dot_prod = (parts * whole).sum(-1)
     return dot_prod

contribution_tracing/utils.py
@@ -1,20 +1,61 @@
 import torch
+from typing import Union, Tuple
 
-def get_top_x_contribution_values(contributions: torch.Tensor, top_x: float) -> torch.Tensor:
+@torch.no_grad()
+def get_top_x_contribution_values(contributions: torch.Tensor, top_x: float, min_total_threshold: float =1.0) -> torch.Tensor:
 
-    sorted_values, sorted_idx = torch.sort(contributions, descending=True, dim=-1)
-    cummulative_sum = torch.cumsum(sorted_values.clip(0), dim=-1)
+    net_sum = contributions.sum(dim=-1, keepdim=True)
+    valid_row_mask = net_sum > min_total_threshold
 
-    threshold = sorted_values.sum(dim=-1) * top_x
-    mask = cummulative_sum <= threshold[:, None]
-    mask = mask.roll(shifts=1, dims=(-1))
-    mask[:, 0] = 1
-    filtered_values = torch.where(mask, sorted_values, 0)
+    pos_contributions = torch.clamp(contributions, min=0)
+    sorted_pos_values, sorted_pos_idx = torch.sort(pos_contributions, descending=True, dim=-1)
+    
+    cummulative_sum = torch.cumsum(sorted_pos_values, dim=-1)
 
-    inversed_idx = sorted_idx.argsort(dim=-1)
+    threshold = net_sum * top_x
+    mask = cummulative_sum <= threshold
+    
+    # include the item that crossed the threshold
+    mask = mask.roll(shifts=1, dims=-1)
+    mask[..., 0] = 1 
+
+    final_mask = mask & (sorted_pos_values > 0) & valid_row_mask
+    filtered_values = torch.where(final_mask, sorted_pos_values, 0.0)
+
+    inversed_idx = sorted_pos_idx.argsort(dim=-1)
     filtered_contributions = filtered_values.gather(-1, inversed_idx)
 
     return filtered_contributions
 
+@torch.no_grad()
 def mean_center_tensor(tensor: torch.Tensor) -> torch.Tensor:
     return tensor - tensor.mean(dim=-1, keepdim=True)
+
+@torch.no_grad()
+def trace_through_layer_norm(target: torch.Tensor, norm_W: torch.Tensor, norm_var: torch.Tensor, device: Union[str, torch.device] = 'cuda') -> torch.Tensor:
+    input_device = target.device
+    if not device:
+        device = input_device
+    
+    var_scaled_target = target.to(device) * norm_var[:, None].to(device)
+    norm_w_scaled_target = var_scaled_target / norm_W.to(device)
+
+    return norm_w_scaled_target.to(input_device)
+
+def is_broadcastable(shape1: Tuple[int, ...], shape2: Tuple[int, ...]) -> bool:
+    
+    s1 = list(reversed(shape1))
+    s2 = list(reversed(shape2))
+    
+    max_dims = max(len(s1), len(s2))
+    
+    while len(s1) < max_dims:
+        s1.append(1)
+    while len(s2) < max_dims:
+        s2.append(1)
+        
+    for dim1, dim2 in zip(s1, s2):
+        if dim1 != dim2 and dim1 != 1 and dim2 != 1:
+            return False
+            
+    return True